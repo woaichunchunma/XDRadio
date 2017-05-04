36e6c3d83c2ff83fb7c3114907165ef78691de76
imac <eesuper@126.com>
2017-05-04 13:42:11 +0800

    Initial Commit

---
+++ XDRadio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDRadio
+//
+//  Created by iMac on 17/5/4.
+//  Copyright © 2017年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XDRadio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDRadio
+//
+//  Created by iMac on 17/5/4.
+//  Copyright © 2017年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
